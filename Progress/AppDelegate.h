f3b5a77b7456e2d5cbb521ba2243e869a20c43d7
crazypoo <273277355@qq.com>
2014-08-13 20:25:50 +0800

    Initial Commit

---
+++ Progress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Progress
+//
+//  Created by crazypoo on 14/8/13.
+//  Copyright (c) 2014年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Progress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Progress
+//
+//  Created by crazypoo on 14/8/13.
+//  Copyright (c) 2014年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
