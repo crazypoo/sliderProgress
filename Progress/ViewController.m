765d58d8385dbdc394fb4a4e122f592857b604d9
crazypoo <273277355@qq.com>
2014-08-13 21:32:48 +0800

    sliderProgress
    sliderProgress

--- Progress/ViewController.m
+++ Progress/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSTimer *sliderTimer;
+    UISlider *slider;
+    float i;
+}
 
 @end
 
@@ -17,8 +22,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    slider = [[UISlider alloc] initWithFrame:CGRectMake(0, 100, 200, 10)];
+    [slider setThumbImage:[UIImage imageNamed:@"image_SideHome"] forState:UIControlStateNormal];
+    slider.minimumValue = 0;
+    slider.maximumValue = 100;
+    [self.view addSubview:slider];
+    
+    sliderTimer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(updateSlider) userInfo:nil repeats:YES];
+    i=0;
 }
 
+-(void)updateSlider
+{
+    i++;
+    slider.value = i;
+    if (slider.value == 100) {
+        [sliderTimer invalidate];
+        [slider removeFromSuperview];
+    }
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
